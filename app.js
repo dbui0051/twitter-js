dd2592904ec01e35a8e166bad4524d7cc6fe1354
dbui0051 <dennisd.bui@gmail.com>
2017-06-12 18:00:27 -0400

    seeded bank

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
 const volleyball = require('volleyball');
+const nunjucks = require('nunjucks');
+
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 
 app.use(volleyball);
 
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', { noCache: true });
+
 app.get('/', function(req, res){
-	res.status(200).send('You reached homepage!');
+	res.status(200).render( 'index', {title: 'Hall of Fame', people: people} );
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
